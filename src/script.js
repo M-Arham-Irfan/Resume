4b433c1b2d9c0e2e82c4ba93a1e79afe343b86ab
Muhammad Arham Irfan <157938481+M-Arham-Irfan@users.noreply.github.com>
2024-10-08 23:17:50 +0500

    Setting js

--- src/script.js
+++ src/script.js
@@ -1,28 +1,18 @@
-const themeToggle = document.getElementById("theme-toggle");
-const thememToggle = document.getElementById("theme-mtoggle");
-const html = document.documentElement;
-document.getElementById("mobile-menu").addEventListener("click", toggleMenu);
-document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
-
-themeToggle.addEventListener("click", () => {
-  if (html.classList.contains("dark")) {
-    html.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-  } else {
-    html.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-  }
-});
-thememToggle.addEventListener("click", () => {
-  if (html.classList.contains("dark")) {
-    html.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-  } else {
-    html.classList.add("dark");
-    localStorage.setItem("theme", "dark");
+// ReadyState Event
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    const initialHash = window.location.hash;
+    if (initialHash) {
+      const initialLink = document.querySelector(`a[href="${initialHash}"]`);
+      if (initialLink) {
+        initialLink.classList.add("active");
+      }
+    }
+    initApp();
   }
 });
 
+// Mobile Menu Toggle
 const toggleMenu = () => {
   var navbar = document.getElementById("mobile-menu");
   if (navbar.classList.contains("flex")) {
@@ -34,37 +24,56 @@ const toggleMenu = () => {
   }
 };
 
-if (localStorage.getItem("theme") === "dark") {
-  html.classList.add("dark");
-} else {
-  html.classList.remove("dark");
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const header = document.getElementById("header");
-  const navbarLinks = document.querySelectorAll("#navbari a");
-
-  const setActiveLink = (event) => {
-    navbarLinks.forEach((link) => link.classList.remove("active"));
-    event.currentTarget.classList.add("active");
-  };
-
-  navbarLinks.forEach((link) => link.addEventListener("click", setActiveLink));
-
-  const initialHash = window.location.hash;
-  if (initialHash) {
-    const initialLink = document.querySelector(`a[href="${initialHash}"]`);
-    if (initialLink) {
-      initialLink.classList.add("active");
-    }
+// Transparent Header on Scrolling
+const addHeaderBackground = () => {
+  if (window.scrollY > 10) {
+    header.classList.add("bg-scrolled");
+  } else {
+    header.classList.remove("bg-scrolled");
   }
-  const addHeaderBackground = () => {
-    if (window.scrollY > 10) {
-      header.classList.add("bg-scrolled");
-    } else {
-      header.classList.remove("bg-scrolled");
-    }
-  };
+};
 
+const setLinks = () => {
+  const navbarLinks = document.querySelectorAll("#navbari a");
+  navbarLinks.forEach((link) =>
+    link.addEventListener("click", (event) => {
+      navbarLinks.forEach((link) => link.classList.remove("active"));
+      event.currentTarget.classList.add("active");
+    })
+  );
+};
+// Work Flow Function
+const initApp = () => {
+  setLinks();
+
+  const themeToggle = document.getElementById("theme-toggle");
+  const thememToggle = document.getElementById("theme-mtoggle");
+  const html = document.documentElement;
+  document.getElementById("mobile-menu").addEventListener("click", toggleMenu);
+  document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
+
+  // Window Event Listener for Transparen Header
   window.addEventListener("scroll", addHeaderBackground);
-});
+
+  // Theme Toggle pc
+  themeToggle.addEventListener("click", () => {
+    if (html.classList.contains("dark")) {
+      html.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      html.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+
+  // Theme Toggle mobile
+  thememToggle.addEventListener("click", () => {
+    if (html.classList.contains("dark")) {
+      html.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      html.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+};
